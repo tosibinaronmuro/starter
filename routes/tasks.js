12b9d6e3d5e5305506df35c988870b6a25bab1a3
Tosibinaronmuro <tosironj@gmail.com>
2023-01-30 17:11:28 +0100

    minor refactoring to the task routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,11 +2,9 @@ const express=require('express')
 const { getAllTasks, createNewTask, getSingleTask, updateTask, deleteTask } = require('../functions/taskfunction')
 const router=express.Router()
 
-router.route('/').get(getAllTasks)
-router.route('/').post(createNewTask)
-router.route('/:id').get(getSingleTask)
-router.route('/:id').patch(updateTask)
-router.route('/:id').delete(deleteTask)
+router.route('/').get(getAllTasks).post(createNewTask)
+router.route('/:id').get(getSingleTask).patch(updateTask).delete(deleteTask)
+ 
  
 
 
